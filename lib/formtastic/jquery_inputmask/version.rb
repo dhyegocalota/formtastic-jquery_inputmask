96fef90aceeca7e8f61df71bf939f30f221119ce
Dhyego Fernando <dhyegofernando@gmail.com>
2017-10-13 16:50:43 -0400

    Bump up to 0.2.0

--- lib/formtastic/jquery_inputmask/version.rb
+++ lib/formtastic/jquery_inputmask/version.rb
@@ -2,8 +2,8 @@ module Formtastic
   module JqueryInputmask
     module Version
       MAJOR = 0
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
