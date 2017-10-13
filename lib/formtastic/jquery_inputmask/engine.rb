741bab60040eeed6a9eec07f824cb000b6ea9342
Dhyego Fernando <dhyegofernando@gmail.com>
2017-10-13 09:56:58 -0400

    Add bower dependency support

--- lib/formtastic/jquery_inputmask/engine.rb
+++ lib/formtastic/jquery_inputmask/engine.rb
@@ -2,7 +2,7 @@ module Formtastic
   module JqueryInputmask
     class Engine < ::Rails::Engine
       initializer 'formtastic.assets.paths' do |app|
-        app.config.assets.paths << File.join(File.dirname(__FILE__), '../../../vendor/Inputmask/dist')
+        app.config.assets.paths << File.join(File.dirname(__FILE__), '../../../vendor/inputmask/dist')
       end
     end
   end
